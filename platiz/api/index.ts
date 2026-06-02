b397c1b577faeeea78fdbdd8aaaa5436921d914d
jcespinoza2011 <jcespinoza2011@gmail.com>
2026-06-02 01:07:41 -0400

    fix: registrar settings routes en api/index.ts

--- platiz/api/index.ts
+++ platiz/api/index.ts
@@ -1,5 +1,7 @@
 import app from '../backend/src/index';
 import partnerRoutes from '../backend/src/routes/partnerRoutes';
+import settingsRoutes from '../backend/src/routes/settingsRoutes';
 app.use('/api/partners', partnerRoutes);
+app.use('/api/settings', settingsRoutes);
 export default app;
 
