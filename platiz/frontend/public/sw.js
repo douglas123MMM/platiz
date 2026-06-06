c3be25488349471e736c802124b465c534c93f9f
jcespinoza2011 <jcespinoza2011@gmail.com>
2026-06-05 20:57:51 -0400

    PWA: instalar como app en Android y iPhone

---
+++ platiz/frontend/public/sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'gdorado-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/login','/soporte'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
